af33e85bc75e4572efc4d96d2664675ae2e9928b
Deshen Padayachee <deshenx@gmail.com>
2018-02-15 19:31:57 +0200

    Added first component

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,3 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 
+import App from './components/App';
+
+render(<App />, document.querySelector("#main"));
