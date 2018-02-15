7cbfef0c861f4465f4afd3ffb84061a27c445a0f
Deshen Padayachee <deshenx@gmail.com>
2018-02-15 19:44:49 +0200

    Setting up some packages to be added

---
+++ todo/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	render() {
+		return(
+			<div>
+				<div>
+						The main app
+				</div>
+			</div>
+		)	
+	}
+}
+
+export default App;
