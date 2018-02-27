d2a0517046c05d92941a21eac1f41dbc10b85a5a
Deshen Padayachee <deshenx@gmail.com>
2018-02-27 10:05:38 +0200

    Added sample state for tasks * removed Todo.js * added loadSamples function * displayed task details on the screen

--- todo/src/components/App.js
+++ todo/src/components/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import Header from './Header';
-//import Sample from '../sample';
-import Todo from './Todo';
+import Sample from '../sample';
+import Task from './Task';
 
 class App extends React.Component {
+  //this constructor is used to initialise the state
+  constructor(){
+    super();
+
+    this.loadSamples = this.loadSamples.bind(this);
+
+    this.state = {
+      tasks: {}
+    }
+  }
+
+  loadSamples(){
+    this.setState({
+      tasks: Sample
+    })
+  }
+
   render() {
     return(
       <div>
         <Header />
-        App page
-        <Todo />
+        <button onClick={this.loadSamples}>Sample</button>
+        <ul className="task-list">
+          {
+            Object
+              .keys(this.state.tasks)
+              .map(key => <Task key={key} details={this.state.tasks[key]}/>)
+          }
+        </ul>
       </div>
     )	
   }
--- todo/src/components/Task.js
+++ todo/src/components/Task.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 class Task extends React.Component {
   render() {
+
+    const {details} = this.props; //This is the destructured version of
+    //const details = this.props.details;
+
     return(
       <li className="task-card">
-        <h2>Task name</h2>
-        <section>Task details</section>
-        <div>completed: ?</div>
+        <h2>{details.heading}</h2>
+        <section>{details.text}</section>
+        <div>completed: {details.complete ? "true" : "false"}</div>
       </li>
     )	
   }
