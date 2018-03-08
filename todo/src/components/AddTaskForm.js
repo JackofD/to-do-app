8987aca488f563f93680a4f8a946e06d9af831a5
Deshen Padayachee <deshenx@gmail.com>
2018-03-08 14:46:22 +0200

    added temporary required attribute

--- todo/src/components/AddTaskForm.js
+++ todo/src/components/AddTaskForm.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 class AddTaskForm extends React.Component {
 
+  //react 16.3 and onward examples
   //headingRef = React.createRef();
   //detailRef = React.createRef();
 
   createTask = (event) => {
     event.preventDefault();
-    //console.log(this.headingRef.value.value);
 
     const task = {
       heading: this.heading.value,
@@ -23,7 +23,7 @@ class AddTaskForm extends React.Component {
     return(
       <form ref={(input) => (this.taskForm = input)} action="" className="task-card" onSubmit={this.createTask}>
         <h3 className="task-card__heading">Add new</h3>
-        <input name="task-heading" ref={(input) => (this.heading = input)} placeholder="Task heading" id="task-heading" />
+        <input name="task-heading" required ref={(input) => (this.heading = input)} placeholder="Task heading" id="task-heading" />
         <textarea name="task-detail" ref={(input) => (this.detail = input)} placeholder="Task details" id="task-detail"></textarea>
         <button name="submit-button">Add</button>
       </form>
